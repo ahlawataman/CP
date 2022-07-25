af17a84aae9bab4e9bac10a7505361e4fafc224d
Aman Ahlawat <24404559+ahlawataman@users.noreply.github.com>
2022-07-25 13:44:11 +0530

    Update sort 0,1,2.cpp

--- sort 0,1,2.cpp
+++ sort 0,1,2.cpp
@@ -12,9 +12,9 @@ public:
         int mid = 0; // elements [low, mid-1] are 1's
         int high = nums.size()-1; // elements right to high are 2's
         while(mid<=high) {
-            if(nums[mid] == 0) swap(nums[low++], nums[mid++]);
-            else if(nums[mid] == 2) swap(nums[mid], nums[high--]);
-            else mid++;
+            if(nums[mid] == 0) swap(nums[low++], nums[mid++]); // if we find 0, swap it with left
+            else if(nums[mid] == 2) swap(nums[mid], nums[high--]); // if we find 2, swap it with right
+            else mid++; // if we find 1, move ahead
         }
     }
 };
