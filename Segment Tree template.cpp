385a0e5c190c8c017f378b077c34c4a497983154
Aman Ahlawat <24404559+ahlawataman@users.noreply.github.com>
2022-08-07 20:20:53 +0530

    Update Segment Tree template.cpp

--- Segment Tree template.cpp
+++ Segment Tree template.cpp
@@ -1,3 +1,13 @@
+int getMax(vector<int> &sT, int i, int j, int l, int r, int index) {
+    // part of segment
+    if(l<=i && r>=j) return sT[index];
+    int mid = i + (j-i)/2;
+    // excludes
+    if(r<i || l>j) return 0;
+    // overlaps
+    return max(getMax(sT, i, mid, l, r, 2*index+1), getMax(sT, mid+1, j, l, r, 2*index+2));
+}
+
 void buildTree(vector<int> &sT, vector<int> &v, int i, int j, int index) {
     if(i==j) {
         sT[index] = v[i];
@@ -13,6 +23,6 @@ int main() {
     vector<int> v = {1,3,5,2,4};
     vector<int> sT(v.size()*4, 0);
     buildTree(sT, v, 0, v.size()-1, 0);
-    int l = 0, r = 3; // query
+    int l = 0, r = 1; // query
     cout<<getMax(sT, 0, v.size()-1, l, r, 0); // query function
 }
