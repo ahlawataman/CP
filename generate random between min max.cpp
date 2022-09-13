bb4a9ba16cfb68935ec9c6c4619fafb47956a263
Aman Ahlawat <24404559+ahlawataman@users.noreply.github.com>
2022-09-13 23:36:03 +0530

    Update generate random between min max.cpp

--- generate random between min max.cpp
+++ generate random between min max.cpp
@@ -1,7 +1,7 @@
 int main() {
     int min = 5;
     int max = 25;
-    srand(time(NULL));
+    srand(time(NULL)); // SEED VALUE
     int range = max - min + 1;
     int num = rand() % range + min;
     cout<<num;
